b55a9ae3bd9359bfa9c341c320b9e161bb783ca9
Manfred Steyer <manfred.steyer@gmx.net>
2021-09-07 15:12:57 +0200

    Consuming your Feature Library

--- apps/flight-app/src/app/app.module.ts
+++ apps/flight-app/src/app/app.module.ts
@@ -1,11 +1,18 @@
+// apps/flight-app/src/app/app.module.ts
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
+// You might need to add this line by hand:
+import { FeatureSearchModule } from '@my-project/feature-search';
+
 @NgModule({
+  imports: [BrowserModule, 
+    // Import FeatureSearchModule
+    FeatureSearchModule
+  ],
   declarations: [AppComponent],
-  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
